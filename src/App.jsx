85c8b0e4384bc8c735c46a3ca9aa00352f079cb7
ye-yo <yeyo0x0@gmail.com>
2022-03-07 20:52:57 +0900

    Chore: children 예시 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import theme from './styles/theme';
 import Step from './components/Step';
 import { TOTAL_STEP, TEST_STEP } from './constants/step';
 
+const step = ['첫 번째 STEP', 'Stpe2', '<Step3 />'];
+
 export default function App() {
   const [currentStep, setCurrentStep] = useState(TEST_STEP);
   return (
     <ThemeProvider theme={theme}>
       <StepContext.Provider value={{ totalStep: TOTAL_STEP, currentStep, setCurrentStep }}>
-        <Step></Step>
+        <Step>{currentStep.number && step[currentStep.number - 1]}</Step>
         <GlobalStyle />
       </StepContext.Provider>
     </ThemeProvider>
